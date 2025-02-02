9baf3256a2712ff5aa8053f6a58fa5793b75fddb
Federico Cipriani <yo@hesh.cc>
2025-02-01 23:55:25 -0300

    feat: provide flake devshell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Hacklab Development Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            kubectl
+            kubernetes-helm
+            docker
+            zsh 
+            grml-zsh-config
+            kustomize
+          ];
+
+          shellHook = ''           
+            export SHELL=$(which zsh)
+            export KUSTOMIZE_ENABLE_HELM=true
+            echo "🌀 Welcome to the hacklab project development shell!"
+            echo "Available tools:"
+            echo " - kustomize $(kustomize version)"
+            echo " - kubectl $(kubectl version --client | head -n1)"
+            echo " - helm $(helm version --short)"
+            if [ -t 1 ]; then
+              exec zsh
+            fi
+          '';
+        };
+      }
+    );
+}
